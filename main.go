a5c621c5f41cc73d45758bbef506f8ef5aef874c
HarshVardhana2018 <harsh.vardhana.cs.2018@miet.ac.in>
2024-04-18 13:14:15 +0530

    Second commit

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+)
 
 func main() {
 	fmt.Println("hello world")
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("PORT NOT FOUNT IN ENV")
+	}
+	fmt.Println("Port:", portString)
 }
