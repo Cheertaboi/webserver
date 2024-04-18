a0046369a28f4775b77ff91663fb6bae89b86c82
HarshVardhana2018 <harsh.vardhana.cs.2018@miet.ac.in>
2024-04-18 14:19:24 +0530

    Third commit

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Cheertaboi/webserver
 
 go 1.21.5
+
+require github.com/joho/godotenv v1.5.1 // indirect
--- main.go
+++ main.go
@@ -4,10 +4,15 @@ import (
 	"fmt"
 	"log"
 	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
 	fmt.Println("hello world")
+
+	godotenv.Load(".env")
+
 	portString := os.Getenv("PORT")
 	if portString == "" {
 		log.Fatal("PORT NOT FOUNT IN ENV")
